ba82587ba0c9181afa705212880dd49191f12572
daoanhhung99999999-ctrl <daoanhhung99999999@gmail.com>
2026-09-24 16:02:14 +0700

    chore: clean up temporary script files

--- backend/test-api 4.js
+++
@@ -1,3 +0,0 @@
-const jwt = require('jsonwebtoken');
-const token = jwt.sign({ userId: 'test-admin', role: 'admin' }, 'fallback_secret_key_for_development');
-console.log("Token:", token);
--- backend/test-filters-api 2.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.get('http://localhost:3001/api/products/admin/filters', res => {
-  let body = '';
-  res.on('data', d => body += d);
-  res.on('end', () => console.log(body));
-});
